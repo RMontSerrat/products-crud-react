ff7687075ee069d31c23eae40648255300a9013a
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2024-06-27 09:09:04 -0300

    change header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,11 @@
-import { Typography } from "@mui/material";
 import React from "react";
 
 export function Header({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex w-full my-5 items-center text-center justify-center">
-      <Typography color="black" variant="h3">
+    <div className="flex w-full my-5 items-center justify-center text-center">
+      <h3 className="text-black text-2xl sm:text-4xl font-medium">
         {children}
-      </Typography>
+      </h3>
     </div>
   );
 }
